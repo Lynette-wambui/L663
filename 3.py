2fb411f96739d77b01cea84fb81767ad81cb98ac
LYNETTE WAMBUI <lynettemuraga47@gmail.com>
2025-08-02 17:27:13 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,19 @@
+
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+
+# Calculate the exponent
+result = base ** exponent
+
+# Display the result
+print(f"{base} raised to the power of {exponent} is {result}")
+
+
+base = int(input("Enter the base number: "))
+power = int(input("Enter the power: "))
+
+# Calculate the power
+result = base ** power 
+
+# Display the result
+print(f"{base} raised to the power of {power} is {result}")
